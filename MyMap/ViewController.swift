59fa3f57723af9bae9195819842051b5aa95f205
Cece Soudaly <ttoukta@yahoo.com>
2017-04-13 20:31:02 -0500

    Initial Commit

---
+++ MyMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMap
+//
+//  Created by Cece Soudaly on 4/13/17.
+//  Copyright © 2017 CeceMobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
